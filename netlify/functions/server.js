da5079941c36e31605688f434d2e5c019518baea
Adrian Zinko <zinko.adrian00@gmail.com>
2023-05-01 10:32:19 +0200

    add express server handler

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -6,8 +6,12 @@ import serverless from 'serverless-http';
 const app = express();
 const router = express.Router();
 
+app.use(express.json());
+
 router.get('/mod_03', (req, res) => {
-  res.status(200).json({ output: req.body });
+
+  const resultArray = req.body.map(item => item.result === 'true' ? 1 : 0);
+  res.status(200).send(resultArray);
 });
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
